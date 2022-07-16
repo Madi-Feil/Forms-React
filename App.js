debcc44fb1fde224ccaacd356e85056842c5d1f4
Madi Greeley <98246671+Madi-Feil@users.noreply.github.com>
2022-07-16 16:06:37 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import UserForm from './components/UserForm'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='userForm'>UserForm</h1>
+      <UserForm>
+        
+      </UserForm>
+    </div>
+  );
+}
+
+export default App;
